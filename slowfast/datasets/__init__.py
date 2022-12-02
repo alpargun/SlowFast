3dd40a9681a3d876c0e5783aef36aeefd1e0985e
Alp <alp.argun@ice.rwth-aachen.de>
2022-12-02 03:14:12 +0100

    register bdd dataset

--- slowfast/datasets/__init__.py
+++ slowfast/datasets/__init__.py
@@ -7,6 +7,8 @@ from .charades import Charades  # noqa
 from .imagenet import Imagenet  # noqa
 from .kinetics import Kinetics  # noqa
 from .ssv2 import Ssv2  # noqa
+from .bdd import Bdd
+
 
 try:
     from .ptv_datasets import Ptvcharades, Ptvkinetics, Ptvssv2  # noqa
